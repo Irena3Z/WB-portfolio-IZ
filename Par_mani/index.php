dfa26c18121a217394bb0660d9fa053f0844fee2
Irena3Z <zbitkovski3@gmail.com>
2021-07-11 17:13:50 +0300

    added new changes

---
+++ Par_mani/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="comments/">
+<?php
+include __DIR__. 'comments/index.php>'?>
+
+
+
+<div id="root">
+    <div class="app">
+        <header class="navbar">
+            <div class="container" style="transform: none;">
+                <h1 class="initials">IZ</h1>
+                <ul>
+                    <nav class="nav-options">
+                        <a aria-current="page" class="navbar-text active" href="/">About me</a>
+                        <a class="navbar-text" href="/Par_mani">Gallery</a>
+                        <a class="navbar-text" href="/CV">Resume</a>
+                        <a class="navbar-text" href="/Kontakti">Contacts</a>
+                    </nav>
+                </ul>
+            </div>
+        </header>
+        <div class="intro">
+            <h1 class="intro-text">My hobby is taking pictures. I want to share my gallery with you.</h1>
+            <span class="">
+                <img src="img/1.jpg" alt="My Pic" class="profile-image">
+                <img src="img/2.jpg" alt="My Pic" class="profile-image">
+                <img src="img/3.jpg" alt="My Pic" class="profile-image">
+                <img src="img/4.jpg" alt="My Pic" class="profile-image">
+                <img src="img/5.jpg" alt="My Pic" class="profile-image">
+                <img src="img/6.jpg" alt="My Pic" class="profile-image">
+                <img src="img/7.jpg" alt="My Pic" class="profile-image">
+                <img src="img/8.jpg" alt="My Pic" class="profile-image">
+                <img src="img/9.jpg" alt="My Pic" class="profile-image">
+                <img src="img/10.jpg" alt="My Pic" class="profile-image">
+                <img src="img/11.jpg" alt="My Pic" class="profile-image">
+                <img src="img/12.jpg" alt="My Pic" class="profile-image">
+                <img src="img/13.jpg" alt="My Pic" class="profile-image">
+                <img src="img/14.jpg" alt="My Pic" class="profile-image">
+                <img src="img/15.jpg" alt="My Pic" class="profile-image">
+                <img src="img/16.jpg" alt="My Pic" class="profile-image">
+                <img src="img/17.jpg" alt="My Pic" class="profile-image">
+                <img src="img/18.jpg" alt="My Pic" class="profile-image">
+                <img src="img/19.jpg" alt="My Pic" class="profile-image">
+                <img src="img/20.jpg" alt="My Pic" class="profile-image">
+                <img src="img/21.jpg" alt="My Pic" class="profile-image">
+                <img src="img/22.jpg" alt="My Pic" class="profile-image">
+                <img src="img/23.jpg" alt="My Pic" class="profile-image">
+                <img src="img/24.jpg" alt="My Pic" class="profile-image">
+                <img src="img/25.jpg" alt="My Pic" class="profile-image">
+                <img src="img/26.jpg" alt="My Pic" class="profile-image">
+                <img src="img/27.jpg" alt="My Pic" class="profile-image">
+                <img src="img/28.jpg" alt="My Pic" class="profile-image">
+                <img src="img/29.jpg" alt="My Pic" class="profile-image">
+                <img src="img/30.jpg" alt="My Pic" class="profile-image">
+               
+            </span>
+        </div>
+    </div>
+</div>
+ 
